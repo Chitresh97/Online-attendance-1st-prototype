92aef33e35c81ff04cbe43db309f593595685bb1
chitresh97 <chitu1997@gmail.com>
2018-08-05 11:13:59 +0530

    Mark me present button only enabled when there is a class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:attendence]
   def attendence
     @user=User.find_by(id: current_user.id)
-    @periods=Period.all
+    @today=Period.where(class_date: Time.now.strftime("%a, %d %b %Y ") ).pluck(:class_date).last
+    @periods=Period.all.pluck(:class_date)
   end
 end
